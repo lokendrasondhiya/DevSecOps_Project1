a97c7f20a0833a6d7da95d30907c97b854e40111
lokendra sondhiya <lokendra.sondhiya@gmail.com>
2024-08-14 09:48:26 +0530

    changed app.js descriptiom

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lokendra Cap1project_REVA_RACE_MSc_CAS
         </a>
       </header>
     </div>
